8d855f565f9b3429f045cfe9135f68741a05459f
ian05012 <ian31333025@gmail.com>
2021-08-20 19:21:26 +0800

    configLoader

---
+++ src/main/java/io/github/tinyyana/popsteve/ConfigLoader.java
@@ -0,0 +1,52 @@
+package io.github.tinyyana.popsteve;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class ConfigLoader {
+    private File file;
+    private FileConfiguration configuration;
+
+    private final String fileName;
+    private final PluginMain plugin = PluginMain.getPlugin();
+
+    public ConfigLoader (String fileName) {
+        this.fileName = fileName;
+        this.load();
+    }
+
+    public void load () {
+        this.file = new File(plugin.getDataFolder().getPath(), File.separator + this.fileName + ".yml");
+        this.configuration = new YamlConfiguration();
+
+        if (! file.exists()) {
+            try { plugin.saveResource(this.fileName + ".yml", false); }
+            catch (IllegalArgumentException e) {
+                System.out.println("檔案取得失敗:[" + this.fileName + ".yml]");
+            }
+        }
+
+        try {
+            this.configuration = YamlConfiguration.loadConfiguration(this.file);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("檔案加載失敗:[" + this.fileName + ".yml]");
+        }
+    }
+
+    public boolean save () {
+        try { this.configuration.save(file); }
+        catch (Exception e) {
+            System.out.println("檔案儲存失敗:[ " + this.fileName + ".yml]");
+            return false;
+        }
+        return true;
+    }
+
+    public FileConfiguration get () {
+        return this.configuration;
+    }
+}
--- src/main/java/io/github/tinyyana/popsteve/PluginMain.java
+++ src/main/java/io/github/tinyyana/popsteve/PluginMain.java
@@ -1,13 +1,18 @@
 package io.github.tinyyana.popsteve;
 
 import io.github.tinyyana.popsteve.Commands.PopSteveMenu;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PluginMain extends JavaPlugin {
 
+    public static PluginMain plugin;
+    public ConfigLoader config;
+
     @Override
     public void onEnable() {
         loadCommands();
+        config = new ConfigLoader("config");
     }
 
     @Override
@@ -18,4 +23,12 @@ public final class PluginMain extends JavaPlugin {
     public void loadCommands() {
         getCommand("popsteve").setExecutor(new PopSteveMenu(this));
     }
+    public void onLoad(){
+        plugin = this;
+    }
+
+    public static PluginMain getPlugin(){
+        return plugin;
+    }
+
 }
