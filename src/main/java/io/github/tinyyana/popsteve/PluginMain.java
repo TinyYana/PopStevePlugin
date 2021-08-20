54bf191e7865028b48bcb05eaf2d2710e41d481d
TinyYana <yanasakuranight@gmail.com>
2021-08-20 19:02:23 +0800

    Add PopSteveMenu class.

--- src/main/java/io/github/tinyyana/popsteve/PluginMain.java
+++ src/main/java/io/github/tinyyana/popsteve/PluginMain.java
@@ -1,5 +1,6 @@
 package io.github.tinyyana.popsteve;
 
+import io.github.tinyyana.popsteve.Commands.PopSteveMenu;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PluginMain extends JavaPlugin {
@@ -7,11 +8,14 @@ public final class PluginMain extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void loadCommands() {
+        getCommand("popsteve").setExecutor(new PopSteveMenu(this));
     }
 }
