80c7df6370fd0457ee5bb99bbac4c79b19bc3718
ian05012 <ian31333025@gmail.com>
2021-08-20 19:53:15 +0800

    gui

--- src/main/java/io/github/tinyyana/popsteve/Listener/onInventoryClickEvent.java
+++ src/main/java/io/github/tinyyana/popsteve/Listener/onInventoryClickEvent.java
@@ -1,10 +1,12 @@
 package io.github.tinyyana.popsteve.Listener;
 
+import io.github.tinyyana.popsteve.MapManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class onInventoryClickEvent implements Listener {
+    MapManager mapManager = new MapManager();
 
 
     @EventHandler
@@ -14,7 +16,7 @@ public class onInventoryClickEvent implements Listener {
         }
         event.setCancelled(true);
         if(event.getSlot() == 13){
-
+            mapManager.totalPop++;
         }
     }
 
---
+++ src/main/java/io/github/tinyyana/popsteve/MapManager.java
@@ -0,0 +1,6 @@
+package io.github.tinyyana.popsteve;
+
+public class MapManager {
+
+    public int totalPop;
+}
