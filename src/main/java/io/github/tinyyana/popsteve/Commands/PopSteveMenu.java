ec6b8f0df214ca6285ee950bd3ed297d0c5f0cc3
TinyYana <yanasakuranight@gmail.com>
2021-08-20 19:22:43 +0800

    Create gui var.

--- src/main/java/io/github/tinyyana/popsteve/Commands/PopSteveMenu.java
+++ src/main/java/io/github/tinyyana/popsteve/Commands/PopSteveMenu.java
@@ -1,10 +1,13 @@
 package io.github.tinyyana.popsteve.Commands;
 
 import io.github.tinyyana.popsteve.PluginMain;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class PopSteveMenu implements CommandExecutor {
     public PopSteveMenu(PluginMain pluginMain) {
@@ -13,6 +16,7 @@ public class PopSteveMenu implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
+        Inventory gui = Bukkit.createInventory(player,9, ChatColor.BOLD + "Pop Steve :)");
 
         if (!(sender instanceof Player)) {
             sender.sendMessage("Only player can execute this command!");
