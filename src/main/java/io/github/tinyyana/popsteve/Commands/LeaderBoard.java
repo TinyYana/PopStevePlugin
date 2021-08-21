23f26d7b3a9bac44d6548925b7447af499ad561f
TinyYana <yanasakuranight@gmail.com>
2021-08-21 18:53:56 +0800

    18:53 progress

--- src/main/java/io/github/tinyyana/popsteve/Commands/LeaderBoard.java
+++ src/main/java/io/github/tinyyana/popsteve/Commands/LeaderBoard.java
@@ -1,9 +1,14 @@
 package io.github.tinyyana.popsteve.Commands;
 
+import io.github.tinyyana.popsteve.MapManager;
 import io.github.tinyyana.popsteve.PluginMain;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LeaderBoard implements CommandExecutor {
     public LeaderBoard(PluginMain pluginMain) {
@@ -11,6 +16,11 @@ public class LeaderBoard implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+        Player player = (Player) sender;
+        MapManager mapManager = new MapManager();
+        List<Integer> popCount = new ArrayList<>(mapManager.totalPopMap.values());
+        popCount.forEach(System.out::println);
+
+        return true;
     }
 }
