ceba9afb9b3a52f19efbd209039972d4b7f26b88
ian05012 <ian31333025@gmail.com>
2021-08-22 17:26:02 +0800

    test

--- src/main/java/io/github/tinyyana/popsteve/Commands/LeaderBoard.java
+++ src/main/java/io/github/tinyyana/popsteve/Commands/LeaderBoard.java
@@ -8,6 +8,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 
@@ -19,13 +21,29 @@ public class LeaderBoard implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
         sender.sendMessage(ChatColor.BLUE + "--------------§d§lPop Steve§r§9--------------");
         sender.sendMessage(ChatColor.GREEN + "Top 10 Pop");
         sender.sendMessage(ChatColor.GREEN + "Total Pops: " + mapManager.totalPop);
+
+        List<Integer> popCount = new ArrayList<>(mapManager.totalPopMap.values());
+
+        sender.sendMessage(ChatColor.BLUE + "--------------§d§lPop Steve--------------");
+        int i = 0;
         for (UUID uuid : mapManager.getList()) {
+            if(i == 10){
+                break;
+            }
             sender.sendMessage("§61. §e" + Bukkit.getPlayer(uuid).getName() + " §8Count: §7" + mapManager.getPop(uuid));
+            i++;
         }
         sender.sendMessage(ChatColor.BLUE + "-----------------------------------------");
+
+
+        popCount.forEach(System.out::println);
+
+
+
         return true;
     }
 }
